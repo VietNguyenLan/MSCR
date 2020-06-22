5f4c18d5218e9372ab07dafd47614eb16877fd68
cong anh <nguyenanhyoung@gmail.com>
2020-06-22 21:11:17 +0700

    Add project files.

---
+++ Project/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using
+using Project.EF;
+
+namespace Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authourise(user user)
+        {
+            using (OrderSystemEntities od = new OrderSystemEntities())
+            {
+                var userDetails = od.users.Where(x => x.username == user.username && x.password == user.password).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    user.
+                }
+            }
+
+            return;
+        }
+
+    }
+}
