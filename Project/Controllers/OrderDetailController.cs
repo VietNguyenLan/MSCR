39c297fd8394e000428269cc94e7e4523357a530
VietNguyen <nguyenlanviet96@gmail.com>
2020-07-31 21:01:49 +0700

    Create Home for display menu, all order, order detail

---
+++ Project/Controllers/OrderDetailController.cs
@@ -0,0 +1,36 @@
+﻿using Project.EF;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        // GET: OrderDetail
+        public ActionResult Index(order o)
+        {
+            using (OrderSystemEntities1 db = new OrderSystemEntities1())
+            {
+                List<order_detail> _Details = new List<order_detail>();
+                _Details = db.order_detail.Where(x => x.orderID == o.id).ToList();
+                List<product> _products = new List<product>();
+                foreach (order_detail item in _Details)
+                {
+
+                    _products.Add((product)db.products.Where(x => x.id == item.productID));
+                }
+
+
+                dynamic model = new ExpandoObject();
+                model.order = o;
+                model.order_detail = _Details;
+                model.products = _products;
+                return View(model);
+            }
+        }
+    }
+}
