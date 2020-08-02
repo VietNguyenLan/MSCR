ae0576d33c74b4e862704b5447c269f412eb3753
cong anh <nguyenanhyoung@gmail.com>
2020-08-02 16:33:29 +0700

    Công anh 2/8

--- Project/Controllers/RegisterController.cs
+++ Project/Controllers/RegisterController.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 using Project.EF;
+using Project.ViewModels;
 
 namespace Project.Controllers
 {
@@ -24,21 +29,27 @@ namespace Project.Controllers
             var result = (from row in db.users
                           where row.username == user.username
                           select row).ToList();
-          
+
+            //var custemail = (from row in db.users
+            //                 where row.email == user.email
+            //                 select row).ToList();
+
+            user.email_verified = false;
 
             if (result.Count() != 0)
             {
                  user.LoginErrorMsg = "Username Is Already In Use";
 
             }
+            //if (custemail.Count() != 0)
+            //{
+            //    user.LoginErrorMsg = "Email is already in use";
+            //}
 
-            
-             
            else
-            {
-                try
-                {
-                user u = new user();
+           {
+               
+                var u = new user();
                 string path = UpLoadImage(picture);
 
                     u.name = user.name;
@@ -52,19 +63,64 @@ namespace Project.Controllers
                     u.is_active = user.is_active;
                 
                     db.users.Add(u);
-                db.SaveChanges();
-                user.LoginErrorMsg = "data added";
-                return RedirectToAction("Index", "Login");
-                }
-                catch(Exception ex)
-                {
-                    
-                }
+                    db.SaveChanges();
+                    SendActivationEmail(u);
+                    user.LoginErrorMsg = "data added";
+               
                 
-
             }
             return View("Index", user);
         }
+
+        private void SendActivationEmail(user user)
+        {
+            Random rand = new Random();
+            int otpcode = rand.Next(minValue: 1000, maxValue: 10000);
+            otp_table otp = db.otp_table.Add(new otp_table
+            {
+                uId = user.id,
+                otp = otpcode,
+                create_time = DateTime.Now
+            });
+            db.SaveChanges();
+
+            using (MailMessage mm = new MailMessage("nguyenanhyoung@gmail.com", user.email))
+            {
+                mm.Subject = "Account Activation";
+                string body = "Hello " + user.username + ",";
+                body += "<br /><br />Thanks for the confirmation and enjoy your day!";
+                body += "<br /><br />Please click the following link to login your account";
+                body += "<br /><a href = '" + string.Format("{0}://{1}/Register/Activation/{2}", Request.Url.Scheme, Request.Url.Authority, otpcode) + "'>Click here !</a>";
+                body += "<br /><br />Thanks";
+                mm.Body = body;
+                mm.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                NetworkCredential NetworkCred = new NetworkCredential("nguyenanhyoung@gmail.com", "anhdaica1");
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 587;
+                smtp.Send(mm);
+            }
+        }
+
+        public ActionResult Activation()
+        {
+            if (RouteData.Values["id"] != null)
+            {
+                int activationCode = Convert.ToInt32(RouteData.Values["id"]);
+                var userActivation = db.users.Where(p => p.otp_table.otp == activationCode).FirstOrDefault();
+                if (userActivation != null)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+
+            return View();
+        }
+
+
 
         public string UpLoadImage(HttpPostedFileBase picture)
         {
---
+++ Project/ViewModels/ForgotEmailViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Project.EF;
+
+namespace Project.ViewModels
+{
+    public class ForgotEmailViewModels
+    {
+
+        [Display(Name = "Email: ")]
+        public string Email { get; set; }
+
+        public string ErrorMgs { get; set; }
+    }
+}
