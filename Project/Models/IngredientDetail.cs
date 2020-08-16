d8cf4405f2dc6814f28f5cf3d80d4eaf0fbea79a
VietNguyen <nguyenlanviet96@gmail.com>
2020-08-16 15:42:04 +0700

    Report For Ingredient back-end

---
+++ Project/Models/IngredientDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Project.EF;
+
+namespace Project.Models
+{
+    public class IngredientDetail
+    {
+        public ingredient Ingredient { get; set; }
+        public int amount { get; set; }
+    }
+}
