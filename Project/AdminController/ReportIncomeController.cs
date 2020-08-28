0b7b134850daa6e75af4dfe0c4bfdbf2688b2c5c
Mạnh Tiến Nguyễn <nmtien2502@gmail.com>
2020-08-29 00:38:37 +0700

    Report Income

---
+++ Project/AdminController/ReportIncomeController.cs
@@ -0,0 +1,25 @@
+﻿using Project.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Project.AdminController
+{
+    public class ReportIncomeController : Controller
+    {
+        // GET: ReportIncome
+        public ActionResult Index()
+        {
+            using (OrderSystemEntities2 db = new OrderSystemEntities2())
+            {
+                DateTime date = DateTime.Now.Date;
+
+                ViewBag.total = db.orders.Where(x => x.take_date == date).Select(i => i.total_price).Sum();
+                return View(db.orders.Include(a => a.user).Include(b => b.service_time).Where(x => x.take_date == date).ToList());
+            }
+        }
+    }
+}
