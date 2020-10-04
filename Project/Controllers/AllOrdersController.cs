b6752f49a9410dac905b978f6c2b32645b59df96
Mạnh Tiến Nguyễn <nmtien2502@gmail.com>
2020-10-04 23:38:37 +0700

    Phan trang all order

--- Project/Controllers/AllOrdersController.cs
+++ Project/Controllers/AllOrdersController.cs
@@ -4,20 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace Project.Controllers
 {
     public class OrderListController : Controller
     {
         // GET: OrderList
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
             using (OrderSystemEntities2 db = new OrderSystemEntities2())
             {
-               
+                if (page == null) page = 1;
+                int pageSize = 10;
+
                 int uid = (Int32)(Session["id"]);
-               
-                return View(db.orders.Where(x => x.userID == uid).ToList());
+                var od = db.orders.Where(x => x.userID == uid).ToList().OrderByDescending(a => a.create_time);
+                int pageNumber = (page ?? 1);
+                return View(od.ToPagedList(pageNumber, pageSize));
             }
         }
     }
