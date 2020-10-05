2ef9a1007c94a8c4e4421b578c38f22c23042675
VietNguyenLan <nguyenlanviet96@gmail.com>
2020-10-05 15:55:55 +0700

    * Web.config: N/A
    * TopUPsController.cs: Change gen top up code algorithm

--- Project/AdminController/TopUPsController.cs
+++ Project/AdminController/TopUPsController.cs
@@ -58,11 +58,12 @@ namespace Project.AdminController
 
                     using (OrderSystemEntities2 db = new OrderSystemEntities2())
                     {
+                        int countNum = db.topup_card.Count();
                         int uID = (Int32)(Session["id"]);
                         string seri = Get8CharacterRandomString();
                         string code = Get8CharacterRandomString();
-                        topup_Card.serial_number = seri;
-                        topup_Card.code = code;
+                        topup_Card.serial_number = countNum + seri;
+                        topup_Card.code = countNum + code;
                         topup_Card.create_time = DateTime.Now;
                         topup_Card.creator = uID;
                         db.topup_card.Add(topup_Card);
