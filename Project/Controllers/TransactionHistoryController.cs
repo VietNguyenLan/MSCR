6b8dc27f7995ba4e0c87537a15b59e7dda5a834a
Mạnh Tiến Nguyễn <nmtien2502@gmail.com>
2020-10-08 00:04:03 +0700

    Phan trang Order , Order theo ngay

--- Project/Controllers/TransactionHistoryController.cs
+++ Project/Controllers/TransactionHistoryController.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using PagedList;
 
 namespace Project.Controllers
 {
     public class TransactionHistoryController : Controller
     {
         // GET: TransactionHistory
-        public ActionResult History()
+        public ActionResult History(int? page)
         {
             if (Session["id"] == null)
             {
@@ -22,8 +23,13 @@ namespace Project.Controllers
                 using(OrderSystemEntities2 db = new OrderSystemEntities2())
                 {
                     int uID = (int)Session["id"];
+                    if (page == null) page = 1;
+                    int pageSize = 5;
+
+
+                    int pageNumber = (page ?? 1);
                     List<transaction> transactions = db.transactions.Include(a => a.user).OrderByDescending(x => x.time).Where(x => x.userID == uID).ToList();
-                    return View(transactions);
+                    return View(transactions.ToPagedList(pageNumber, pageSize));
                 }
                 
             }
